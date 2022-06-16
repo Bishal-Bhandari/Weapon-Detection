cbea7bc7d83e87d203b378395cec9d975f5b9568
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-06-16 19:20:41 +0200

    install requirements detection on live feed

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    # detections
+    results = model(frame)
+    cv2.imshow('YOLO', np.squeeze(results.render()))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()

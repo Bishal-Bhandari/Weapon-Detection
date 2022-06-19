7407ee11135567fa0d0007a5673aef79d4628e64
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-06-19 10:58:52 +0200

    trained model and used it on live feed

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os.path
+import tkinter
 import torch
+import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model = torch.hub.load('ultralytics/yolov5', 'custom', 'yolov5/runs/train/exp4/weights/last.pt', force_reload=True)
+
+# img = os.path.join('Dataset', 'images', 'sample', '3.jpg')
+# results = model(img)
+# results.print()
+# matplotlib.use('TkAgg')
+# plt.imshow(np.squeeze(results.render()))
+# plt.show()
 
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
@@ -16,3 +26,5 @@ while cap.isOpened():
         break
 cap.release()
 cv2.destroyAllWindows()
+
+# python train.py --img 640 --batch 1 --epochs 3 --data coco128.yaml --weights yolov5s6.pt --cache to train
